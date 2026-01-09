cd19ad76a02b38ca00b1dd3c0308c46698ecf844
Tarek Omar Pacha <t.omar_pacha@me.com>
2026-01-09 17:57:27 +0100

    repetetive requests

--- main.py
+++ main.py
@@ -17,38 +17,68 @@ schreibt sie in MongoDB
 #Die daten werden dann in MongoDB (non-SQL) hinterlegt.
 #Organisation über Docke Compose
 #Storage auf GitHub
-#Code:##########################################
-from httpx import request
-from kafka.protocol import API_KEYS
-#Einsatz des API-Key:
 
+
+#Code:##########################################
 import pandas as pd
 import requests
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+import time
 import openpyxl
+from httpx import request
+from kafka.protocol import API_KEYS
 
-load_dotenv()
+#PFADE----------------------------------------
+load_dotenv(Path("C:\GitHub\IU_Data_Engineering\.venv\key.env"))
 print("Script um Die Daten via API-Key von OpenAQ zu laden")
 url = "https://api.openaq.org/v3/locations/4794" #Metadaten Location
 URL = "https://api.openaq.org/v3/locations/2178/latest"#Sensordaten
 API_KEY = os.getenv("OPENAQ_API_KEY")
 
-#Abfrage regelmäßig itereieren:
-response = requests.get(URL, headers={'X-API-KEy' : API_KEY})
-raw_data = response.json()
-print(raw_data.keys())
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_rows', 100)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_columns', None)
+#Funktionen####################################
+def data_request(request_count, url, api_key):
+    request_count = request_count
+    URL = url
+    API_KEY = api_key
 
-#json zu csv:
-df = pd.DataFrame(raw_data['results'])
-df.to_excel('response_data.xlsx', index = False)
+    print(f"Request Nr {request_count} wird eingeholt")
+    response = requests.get(URL, headers={'X-API-Key': API_KEY})
+    raw_js_data = response.json()
+    print(f"Die Keys der response sind {raw_js_data.keys()}")
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_rows', 100)
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.max_columns', None)
 
-print(df)
+    # json-data zu csv konvertieren:
+    df = pd.DataFrame(raw_js_data['results'])
+    df.to_excel('latest_response_data.xlsx', index=False)
+    print(df)
+
+def request_timer ():
+    sec = 1
+    timer_value = 10 #Testweise 10 Sekunden
+    while timer_value > 0:
+        print(f"{sec}")
+        time.sleep(1)
+        sec += 1
+        timer_value -= 1
+    print(f"Timer abgelaufen: {request_count}")
+
+if __name__ == '__main__':
+    #Abfrage regelmäßig iterjieren
+    request_count = 1
+
+    while True:
+        request_timer()
+        data_request(request_count,URL, API_KEY)
+        request_count +=1
+
+
+
 
 
 
