99d1d700b5011ed04585d3ee396388c2d3c4c4e7
Tarek Omar Pacha <t.omar_pacha@me.com>
2026-01-15 20:13:25 +0100

    analyse_fn ready

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -74,7 +74,7 @@ def analyse_fn():
     print(df_clean_pivot)
 
 #thresholdcheck und spikedetection gemessen an den Unterschieden der Vorwerte
-#Ausgabe mit Streamlit
+#Ausgabe mit Streamlit/Flet
 
 mongoDB = pd.DataFrame()
 while analyse == True:
